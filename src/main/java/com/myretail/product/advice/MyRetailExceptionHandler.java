66ceb7228cf1c7b574f0569cb2db1c16cd7e7431
sonyjtp <sonyjtp@gmail.com>
2018-04-28 01:42:35 -0500

    updated exception handling

--- src/main/java/com/myretail/product/advice/MyRetailExceptionHandler.java
+++ src/main/java/com/myretail/product/advice/MyRetailExceptionHandler.java
@@ -2,6 +2,8 @@ package com.myretail.product.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 //import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,15 +23,25 @@ public class MyRetailExceptionHandler {
 				
 	}
 	
-//	@ExceptionHandler(value=AccessDeniedException.class)
-//	public String handleAccessDeniedException(AccessDeniedException e){
-//		return MyRetailConstants.EXCP_ACCESS_DENIED;
-//	}
+	@ExceptionHandler(value=AccessDeniedException.class)
+	public ResponseEntity<ProductResponse> handleAccessDeniedException(Exception e){
+		return new ResponseEntity<ProductResponse>(createErrorResponse(e), HttpStatus.FORBIDDEN);
+	}
 	
+	@ExceptionHandler(value=UsernameNotFoundException.class)
+	public ResponseEntity<ProductResponse> handleUsernameNotFound(Exception e){
+		return new ResponseEntity<ProductResponse>(createErrorResponse(e), HttpStatus.UNAUTHORIZED);
+	}
 	
-	private ProductResponse createErrorResponse(MyRetailException e) {
-		log.error(e.getErrorMessage(), e.getMessage());
+	private <U extends MyRetailException> ProductResponse createErrorResponse(U e) {
+		log.error(String.format("Exception occurred :%s", e.getMessage()));
 		return new ProductResponse(e.getErrorMessage());
+	}
+	
+	
+	private <T extends Exception> ProductResponse createErrorResponse(T e) {
+		log.error(String.format("Exception occurred :%s", e.getMessage()));
+		return new ProductResponse(e.getMessage());
 	}
 
 }
