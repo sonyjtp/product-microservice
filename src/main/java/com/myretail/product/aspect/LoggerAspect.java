bb88f63d9b96c5630e27e990d4d406fe8da0ed78
sonyjtp <sonyjtp@gmail.com>
2018-04-28 01:41:56 -0500

    updated logger aspect

--- src/main/java/com/myretail/product/aspect/LoggerAspect.java
+++ src/main/java/com/myretail/product/aspect/LoggerAspect.java
@@ -3,6 +3,7 @@ package com.myretail.product.aspect;
 import java.text.MessageFormat;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -17,12 +18,18 @@ public class LoggerAspect {
 	@Pointcut("execution(* com.myretail.*.*.*(..))")
 	private void executionJoinPoint() {}
 	
+	@Around("executionJoinPoint()")
 	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-//		if(log.isDebugEnabled())
+//		if(log.isDebugEnabled()){
+		long start = System.currentTimeMillis();
 			log.info(MessageFormat.format("Entering method: {0}", joinPoint.getSignature().toShortString()));
-		Object retVal = joinPoint.proceed();
+//	}
+			Object retVal = joinPoint.proceed();
 //		if(log.isDebugEnabled()) {
+			
 			log.info(MessageFormat.format("Return value: {0}", retVal.toString()));
+			long executionTime = System.currentTimeMillis() - start;
+			log.info(joinPoint.getSignature() + " executed in " + executionTime/1000 + " s");
 			log.info(MessageFormat.format("Exiting method: {0}", joinPoint.getSignature().toShortString()));
 			
 //		}
